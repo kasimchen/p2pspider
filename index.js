e016495fb595d44b49d0f47770eadada22ec335e
kasimchen <746215017@qq.com>
2017-09-06 22:48:20 -0700

    update

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ p2p.on('metadata', function (metadata) {
 
 
     console.log(metadata.info.name.toString());
-    //console.log(metadata.info.pieces.toString());
+    console.log(metadata.info.pieces.toString());
     console.log(metadata.info);
 
 
